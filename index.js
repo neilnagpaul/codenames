ee3f3fdb7f3baadfeeacc67941fb0a8cf8a86f4e
neilnagpaul <65032886+neilnagpaul@users.noreply.github.com>
2025-02-16 16:37:47 -0500

    Re-format code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import { generate } from "random-words"
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import { generate } from "random-words";
 
 const app = express();
 const server = createServer(app).listen(80);
@@ -11,25 +11,27 @@ const events = new WeakMap();
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
-    const { id } = socket.handshake.query;
-    const room = (socket.join(id), io.sockets.adapter.rooms.get(id));
-    if (!events.has(room)) events.set(room, [[...init()]]);
+  const { id } = socket.handshake.query;
+  const room = (socket.join(id), io.sockets.adapter.rooms.get(id));
+  if (!events.has(room)) events.set(room, [[...init()]]);
 
-    events.get(room).forEach(socket.emit.bind(socket, "event"));
-    socket.on("event", (event) => {
-        events.get(room).push(event);
-        socket.broadcast.to(id).emit("event", event);
-    });
+  events.get(room).forEach(socket.emit.bind(socket, "event"));
+  socket.on("event", (event) => {
+    events.get(room).push(event);
+    socket.broadcast.to(id).emit("event", event);
+  });
 });
 
 function* init() {
-    const colors = [
-        ...Array(9).fill("red"),
-        ...Array(8).fill("blue"),
-        ...Array(7).fill("black"),
-        ...Array(1).fill("purple"),
+  const colors = [
+    ...Array(9).fill("red"),
+    ...Array(8).fill("blue"),
+    ...Array(7).fill("black"),
+    ...Array(1).fill("purple"),
+  ];
+  for (const word of generate(25))
+    yield [
+      word.toUpperCase(),
+      ...colors.splice(Math.floor(Math.random() * colors.length), 1),
     ];
-    for (const word of generate(25))
-        yield [word.toUpperCase(), ...colors.splice(
-            Math.floor(Math.random() * colors.length), 1)]
 }
